32cb5511c0d922724b6acb05b5c1d796f7763e52
Kevin Martinez <kevin.martinez@dnamic.ai>
2020-05-10 16:07:17 -0600

    Add server js config

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}!`);
+});
