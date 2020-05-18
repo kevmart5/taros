fdfff2c424cecf58d70b9e78021154c05bddd0f7
Kevin Martinez <kevin.martinez@dnamic.ai>
2020-05-17 18:30:00 -0600

    Change the enviroment url for prod and dev

--- src/constants/backend/index.js
+++ src/constants/backend/index.js
@@ -2,11 +2,11 @@ export const DEVELOMENT_PORT_URL = 'http://localhost:3120';
 export const PRODUCTION_PORT_URL = 'https://backendtarogarage.herokuapp.com';
 
 export const getUrlApi = () => {
-	// const url =
-	// 	process.env.NODE_ENV === 'development'
-	// 		? DEVELOMENT_PORT_URL
-	// 		: PRODUCTION_PORT_URL;
-	const url = DEVELOMENT_PORT_URL;
+	const url =
+		process.env.NODE_ENV === 'development'
+			? DEVELOMENT_PORT_URL
+			: PRODUCTION_PORT_URL;
+	// const url = DEVELOMENT_PORT_URL;
 	return url;
 };
 
